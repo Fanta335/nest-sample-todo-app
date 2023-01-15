c1933482e50b7a6a09e62ef1f60ebafb95f46bd0
Fanta335 <fantayusuke5210@gmail.com>
2023-01-15 23:02:57 +0900

    Add config module & typeorm module imports #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from './config/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [ormconfig],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
