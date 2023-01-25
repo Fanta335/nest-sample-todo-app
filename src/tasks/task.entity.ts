cc6468d98dbcfc6ea70f1ee11131cfc4d3e6199c
Fanta335 <fantayusuke5210@gmail.com>
2023-01-25 18:26:38 +0900

    Change yarn to npm (#29)

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { User } from './../../src/users/user.entity';
+import { User } from '../users/user.entity';
 
 @Entity('tasks')
 export class Task {
